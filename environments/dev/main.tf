297202609720be0f858237e2a1eb87b9003b098a
kyeswanth <yeswanthk@biarca.com>
2021-02-25 20:09:19 +0530

    Updated

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -27,14 +27,3 @@ module "vpc" {
   env     = "${local.env}"
 }
 
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
-
-module "firewall" {
-  source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
