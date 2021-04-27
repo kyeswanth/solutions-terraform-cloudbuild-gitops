5d7f64c2b9ccae8f5326ab5d8ef3cc1f1b9b4ec4
kyeswanth <yeswanthk@biarca.com>
2021-04-27 14:22:21 +0530

    Updated

--- dev/terraform/variables.tf
+++ dev/terraform/variables.tf
@@ -11,5 +11,5 @@ variable "project_id" {
 # vpc network name
 variable "vpc_network_name" {
   type = string
-  default = "infrastructure-dev-network-dev-apr26"
+  default = "infrastructure-dev-network-dev-apr27"
 }
